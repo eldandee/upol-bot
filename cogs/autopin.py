fdb2d3cb2877ab7aa1a083d2955d2392e607bbd8
Miroslav Janska <39723472+eldandee@users.noreply.github.com>
2020-07-28 09:43:02 +0200

    Adds autopin functionality

---
+++ cogs/autopin.py
@@ -0,0 +1,54 @@
+import datetime
+import discord
+from discord.ext import commands
+
+from config import Config
+
+
+class AutoPin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        """
+        if the message has X or more 'pin' emojis pin the message
+        """
+        channel = self.bot.get_channel(payload.channel_id)
+        if channel is None:
+            return
+        try:
+            message = await channel.fetch_message(payload.message_id)
+        except discord.errors.NotFound:
+            return
+        emoji = payload.emoji.name
+        if emoji == '📌':
+            for reaction in message.reactions:
+                if reaction.emoji == '📌' and \
+                   reaction.count >= Config.pin_count and \
+                   not message.pinned and \
+                   message.channel.id not in Config.pin_banned_channels:
+                    users = await reaction.users().flatten()
+                    await self.log(message, users)
+                    await message.pin()
+                    await message.clear_reaction('📌')
+                    break
+
+    async def log(self, message, users):
+        """
+        Logging message link and users that pinned message
+        """
+        embed = discord.Embed(title="📌 Auto pin message log", color=0xeee657)
+        user_names = ', '.join([user.name for user in users])
+        message_link = message.jump_url
+        embed.add_field(name="Users", value=user_names)
+        embed.add_field(name="In channel", value=message.channel)
+        embed.add_field(name="Message", value=message_link, inline=False)
+        embed.timestamp = datetime.datetime.now(tz=datetime.timezone.utc)
+        channel = self.bot.get_channel(Config.log_channel)
+        await channel.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(AutoPin(bot))
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     welcome_channel: int = 735899091371819032
     student_role: str = 'Student'
 
-    cogs = ['help', 'admin', 'calendar', 'weather']
+    cogs = ['help', 'admin', 'calendar', 'weather','autopin']
 
     weather_token = '65b5078cfd3c4d143c2fa40d34377ef1'
     default_city = 'Olomouc'
@@ -15,3 +15,7 @@ class Config:
         '?q={}&units=metric&lang=cz&appid={}'
 
     guild_id: int = 0
+
+     # Pin emoji count to pin
+    pin_count: int = 5
+    pin_banned_channels = [734420885724856431,734420846172438588,734419921491984474,734420918079848449,734421004021137468]
