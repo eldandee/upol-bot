d06d69a51305543760747c900a2626e39a5b1824
MichalHalabica <m.halabica@gmail.com>
2020-07-28 08:09:41 +0200

    Removed list type from config template

--- config.template.py
+++ config.template.py
@@ -1,13 +1,10 @@
-from typing import List
-
-
 class Config:
     token: str = ''
     prefix: str = '!'
     welcome_channel: int = 735899091371819032
     student_role: str = 'Student'
 
-    cogs: List[str] = ['help', 'admin', 'calendar', 'weather']
+    cogs = ['help', 'admin', 'calendar', 'weather']
 
     weather_token = '65b5078cfd3c4d143c2fa40d34377ef1'
     default_city = 'Olomouc'
